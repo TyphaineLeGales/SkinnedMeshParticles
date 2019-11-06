d2dae05a2ed8735adfd2ab385ad05fa3b2514110
TyphaineLeGales <typhaine.le.gales@lusion.co>
2019-11-06 16:10:16 +0000

    fire particles following mouse

---
+++ Assets/KinectToolkitScenes/ConcreteMinimal/Scripts/MouseMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10f; // Set this to be the distance you want the object to be placed in front of the camera.
+        this.transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+        
+    }
+}
