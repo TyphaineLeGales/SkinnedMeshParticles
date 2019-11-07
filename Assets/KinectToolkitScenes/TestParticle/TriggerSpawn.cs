377719c69116dd4a9d5d007355aadbe3918e77fa
TyphaineLeGales <typhaine.le.gales@lusion.co>
2019-11-07 09:13:44 +0000

    test fire particles

---
+++ Assets/KinectToolkitScenes/TestParticle/TriggerSpawn.cs
@@ -0,0 +1,36 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.VFX;
+
+// public class TriggerSpawn : MonoBehaviour
+
+  
+// {
+//       [Serialized Field] VisualEffect _vfx = null;
+//       [Serialized Field] Camera _camera = null;
+
+//       VFXEventAttribute _attrib;
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+        
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         if(Input.GetMouseButtonDown(0)) {
+//             var mousePos = Input.mousePosition;
+//             mousePos.z = _vfx.transform.position - _camera.transform.position.z;
+//             mousePos = _camera.ScreenToWorldPoint(pos);
+
+//             if(_attrib == null) _vfx.CreateVFXEventAttribute();
+//             _attrib.setVector3('position', pos);
+
+//             _vfx.SendEvent("onManualSpawn", _attrib);
+
+//         }
+        
+//     }
+// }
