a88cc04358da5e1ee82eb838512214723e552876
TyphaineLeGales <typhaine.le.gales@lusion.co>
2019-11-11 13:24:51 +0000

    mesh to sdf asset start

---
+++ Assets/KinectToolkitScenes/particleSDF/SDFGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SDFGenerator : MonoBehaviour
+{
+    [SerializeField] Mesh _sourceMesh = null;
+    [SerializeField] RenderTexture _SDF = null;
+   
+
+    void Start()
+    {
+       
+    }
+
+}
